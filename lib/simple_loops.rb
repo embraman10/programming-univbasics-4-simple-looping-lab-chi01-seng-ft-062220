a8900a2faa970bd18520ea0f2531f5e888941bae
Emily Braman <embraman10@gmail.com>
2020-06-13 22:05:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
 # Write your methods here
+def loop_message_five_times
+  puts "Hello World"
+end
+
+def loop_message_n_times
+  puts "Hellow Moon"
+end
+
+def output_array
+
+end
+
+def return_string_array
+
+end
