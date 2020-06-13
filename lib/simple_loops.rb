c0e3100939e0dedfaf1914d4033dc4e2d9411c86
Emily Braman <embraman10@gmail.com>
2020-06-13 22:07:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,15 @@
 # Write your methods here
 def loop_message_five_times
-  puts "Hello World"
+  count = 0
+  while count <5 do
+    puts "Hello World"
+    count += 1
+
 end
 
 def loop_message_n_times
   puts "Hellow Moon"
+
 end
 
 def output_array
